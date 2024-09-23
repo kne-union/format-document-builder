81fa4fde617062689dce885a071fcbad8577ce67
zhipenglin <sunandmoon001@163.com>
2024-09-23 14:48:22 +0800

    初始化项目

---
+++ doc/base.js
@@ -0,0 +1,9 @@
+const {default:FormatDocumentBuilder} = _FormatDocumentBuilder;
+
+const BaseExample = ()=>{
+    return <div>
+        <FormatDocumentBuilder />
+    </div>;
+};
+
+render(<BaseExample />);
